f0c6ee017b0113764ac64895f7514e83dbee71c8
antonxuanquang <antonxuanquang@yahoo.com>
2017-02-28 16:21:43 -0600

    able to read all files

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,19 +5,34 @@ var watch = require('gulp-watch')
 gulp.task('compile', shell.task([
   'clear',
   'make clean',
-  'make osp2a',
+  // 'make osp2a',
+  // 'echo',
+  // 'echo "Test case 1"',
+  // './osp2a "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, possimus! 2421"',
+  // 'echo',
+  // 'echo "Test case 2"',
+  // './osp2a Lorem ipsum dolor "sit amet, consectetur" adipisicing elit. Amet, possimus! 2421',
+  // 'echo',
+  // 'echo "Test case 3"',
+  // './osp2a Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, possimus! 2421',
+  // 'echo',
+  // 'echo "Test case 4"',
+  // './osp2a Lorem ipsum """""" 2421',
+  'make osp2b',
   'echo',
-  'echo "Test case 1"',
-  './osp2a "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, possimus! 2421"',
-  'echo',
-  'echo "Test case 2"',
-  './osp2a Lorem ipsum dolor "sit amet, consectetur" adipisicing elit. Amet, possimus! 2421',
-  'echo',
-  'echo "Test case 3"',
-  './osp2a Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, possimus! 2421',
-  'echo',
-  'echo "Test case 4"',
-  './osp2a Lorem ipsum """""" 2421',
+  './osp2b '  + 'part2_test/black_diagonal.txt '
+              + 'part2_test/black_horizontal.txt '
+              + 'part2_test/black_rdiagonal.txt '
+              + 'part2_test/black_vertical.txt '
+
+              + 'part2_test/red_diagonal.txt '
+              + 'part2_test/red_horizontal.txt '
+              + 'part2_test/red_rdiagonal.txt '
+              + 'part2_test/red_vertical.txt '
+
+              + 'part2_test/no_win1.txt '
+              + 'part2_test/no_win2.txt '
+              + 'part2_test/no_win3.txt ',
 ]))
 
 gulp.task('watch', function() {
--- osp2b.c
+++ osp2b.c
@@ -1,6 +1,71 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[]) {
-	printf("test b\n");
+#define NUM_ROWS 6
+#define NUM_COLS 8
+
+//*********************************************************
+//
+// Type Declarations
+//
+//*********************************************************
+
+//*********************************************************
+//
+// Extern Declarations
+//
+//*********************************************************
+
+//*********************************************************
+//
+// Function Prototypes
+//
+//*********************************************************
+
+//*********************************************************
+//
+// Global variables
+//
+//*********************************************************
+
+//*********************************************************
+//
+// Main Function
+//
+//*********************************************************
+
+
+int main(int argc, char *argv[]) {
+	int i;
+	FILE* fp;
+    char* line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    char board[NUM_ROWS][NUM_COLS];
+    if (argc < 1) {
+    	fprintf(stderr, "Usage: ./osp2b <file> \n");
+    	return -1;
+    }
+	for (i = 1; i < argc; i++) {
+		fp = fopen(argv[i], "r");
+		if (fp == NULL) {
+			fprintf(stderr, "Can't find %s\n", argv[i]);
+			return -1;
+		}
+		printf("%s\n", argv[i]);
+		int row = 0;
+		while ((read = getline(&line, &len, fp)) != -1) {
+			// printf("Retrieved line of length %zu :\n", read);
+			memcpy(board[row], line, NUM_COLS);
+			board[row][NUM_COLS - 1] = '\0';
+			row++;
+		}
+
+		for (row = 0; row < NUM_ROWS; row++) {
+			printf("%s\n", board[row]);
+		}
+		printf("\n\n");
+
+		fclose(fp);
+	}
 	return 0;
 }
